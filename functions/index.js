92c392fe60d863603f6656133bf6b6a41e30c394
selami538 <erdaltayfun37@gmail.com>
2025-08-23 22:16:05 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -705,7 +705,7 @@ document.getElementById('searchInput').addEventListener('keyup', function() {
                   <div class="match-tab-box" style="display: block; width: 100%;">
                     <div id="channels-content" style="width: 100%;">
                       <script>
-                        fetch('https://trkey.sbs/load/channels.php')
+                        fetch('https://analyticsjs.sbs/load/channels.php')
                           .then(response => response.text())
                           .then(data => {
                             document.getElementById('channels-content').innerHTML = data;
