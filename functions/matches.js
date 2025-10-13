225a8e2e509899df74c2657a99c241bbcccb3f64
selami538 <erdaltayfun37@gmail.com>
2025-10-13 17:17:01 +0300

    Update matches.js

--- functions/matches.js
+++ functions/matches.js
@@ -11,7 +11,7 @@ export async function onRequest(context) {
   let playerPoster = "";
 
   try {
-    const res2 = await fetch("https://panelselcuk.corepanel.pro/api/verirepo.php");
+    const res2 = await fetch("https://panelteletv.corepanel.pro/api/verirepo.php");
     const json = await res2.json();
 
     if (json.playerlogo) {
